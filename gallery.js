731d65fbdf3ce8e6c4b6fcb50541d03deb943ae7
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-10-26 14:18:56 +0530

    second commit

--- gallery.js
+++ gallery.js
@@ -18,7 +18,6 @@ function closeModal1() {
   document.getElementById("myModal1").style.display = "none";
 }
 
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -49,3 +48,102 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+
+function floarplanH(){
+  document.getElementById("myModal11").style.display = "block";
+  console.log("hi");
+}
+
+
+function floarplanH1(){
+  document.getElementById("myModal12").style.display = "block";
+  console.log("hi");
+}
+
+
+function floarplanH2(){
+  document.getElementById("myModal13").style.display = "block";
+  
+}
+
+function closeModal1(){
+  document.getElementById("myModal11").style.display = "none";
+}
+
+function closeModal12(){
+  document.getElementById("myModal12").style.display = "none";
+}
+
+function closeModal13(){
+  document.getElementById("myModal13").style.display = "none";
+}
+
+function floarplanE(){
+  document.getElementById("myModalE").style.display = "block";
+}
+
+function floarplanE1(){
+  document.getElementById("myModalE1").style.display = "block";
+
+}
+
+function floarplanE2(){
+  document.getElementById("myModalE2").style.display = "block";
+}
+
+function floarplanE3(){
+  document.getElementById("myModalE3").style.display = "block";
+}
+
+function closeModalE(){
+  document.getElementById("myModalE").style.display = "none";
+}
+
+function closeModalE1(){
+  document.getElementById("myModalE1").style.display = "none";
+}
+
+function closeModalE2(){
+  document.getElementById("myModalE2").style.display = "none";
+}
+
+function closeModalE3(){
+  document.getElementById("myModalE3").style.display = "none";
+}
+
+
+
+function floarplanS(){
+  document.getElementById("myModalS").style.display = "block";
+}
+
+function floarplanS1(){
+  document.getElementById("myModalS1").style.display = "block";
+
+}
+
+function floarplanS2(){
+  document.getElementById("myModalS2").style.display = "block";
+}
+
+function floarplanS3(){
+  document.getElementById("myModalS3").style.display = "block";
+}
+
+function closeModalS(){
+  document.getElementById("myModalS").style.display = "none";
+}
+
+function closeModalS1(){
+  document.getElementById("myModalS1").style.display = "none";
+}
+
+function closeModalS2(){
+  document.getElementById("myModalS2").style.display = "none";
+}
+
+function closeModalS3(){
+  document.getElementById("myModalS3").style.display = "none";
+}
+
