c7152a820a4a870ca1e8ebd0e2f7b5230f701b5d
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-11-14 12:42:40 +0530

    first commit

---
+++ modal.php
@@ -0,0 +1,18 @@
+<?php
+    if(isset($_POST['submit'])){
+		$name=$_POST['client_name'];
+		$email=$_POST['client_mail_id'];
+		$phone=$_POST['client_phone_number'];
+	
+		$to='livexcellence1@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Brigade cornerstone etopia';
+		$message="Name :".$name."\n"."Phone :".$phone."\n";
+		$headers="From: ".$email;
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
