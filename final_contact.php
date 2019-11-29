c30e32578934dacba6437fa5ac70955ac4f6ad1a
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-11-29 12:50:43 +0530

    second push

--- final_contact.php
+++ final_contact.php
@@ -5,11 +5,13 @@
 		$phone=$_POST['client_phone_number'];
 		$msg=$_POST['client_message'];
 		$to='livexcellence1@gmail.com'; // Receiver Email ID, Replace with your email ID
-		$subject='Brigade Cornerstone etopia';
-		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$subject='Brigade Cornerstone utopia';
+		$message="ProjectName:"."Brigade Cornerstone utopia"."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
 		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+			echo "<h1 style=\"text-align:center\">Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1hCkrTkv0heLHcoDRmEipcJQKKO-H3UY2">Click here to download Brochure</a><h3>';
+			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=10MFgV_joBKJsfkIDIeasShVqMbWbxWyC">Click here to download Cost Sheet</a><h3>';
 		
 		}
 		else{
--- modal.php
+++ modal.php
@@ -6,10 +6,12 @@
 	
 		$to='livexcellence1@gmail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Brigade cornerstone etopia';
-		$message="Name :".$name."\n"."Phone :".$phone."\n";
+		$message="ProjectName:"."Brigade Cornerstone utopia"."\n"."Name :".$name."\n"."Phone :".$phone."\n";
 		$headers="From: ".$email;
 		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+			echo "<h1 style=\"text-align:center\">Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1hCkrTkv0heLHcoDRmEipcJQKKO-H3UY2">Click here to download Brochure</a><h3>';
+			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=10MFgV_joBKJsfkIDIeasShVqMbWbxWyC">Click here to download Cost Sheet</a><h3>';
 		}
 		else{
 			echo "Something went wrong!";
