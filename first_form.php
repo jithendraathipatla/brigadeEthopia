1c53c6b3af22d2639ad7a5ef9b8c3886a2943e02
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-11-29 13:29:01 +0530

    new chang

--- first_form.php
+++ first_form.php
@@ -11,7 +11,7 @@
 		if(mail($to, $subject, $message, $headers)){
 			echo "<h1  style=\"text-align:center\">Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
 			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1hCkrTkv0heLHcoDRmEipcJQKKO-H3UY2">Click here to download Brochure</a><h3>';
-			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=10MFgV_joBKJsfkIDIeasShVqMbWbxWyC">Click here to download Cost Sheet</a><h3>';
+			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1RtcCxtDlIGL8dDt1tZ6WuRFZius52FNR">Click here to download Cost Sheet</a><h3>';
 		}
 		else{
 			echo "Something went wrong!";
